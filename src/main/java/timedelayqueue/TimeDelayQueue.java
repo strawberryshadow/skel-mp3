2e8467e72e063732410e489a48977a6eeb016f8b
strawberryshadow <lilymwatt@gmail.com>
2022-11-28 01:15:58 -0800

    T1 ok, not sure if threadsafe

--- src/main/java/timedelayqueue/TimeDelayQueue.java
+++ src/main/java/timedelayqueue/TimeDelayQueue.java
@@ -1,8 +1,10 @@
 package timedelayqueue;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.UUID;
+import javax.crypto.interfaces.PBEKey;
+import java.sql.Timestamp;
+import java.util.*;
+import java.util.stream.StreamSupport;
+
 
 // TODO: write a description for this class
 // TODO: complete all methods, irrespective of whether there is an explicit TODO or not
@@ -10,7 +12,13 @@ import java.util.UUID;
 // TODO: State the rep invariant and abstraction function
 // TODO: what is the thread safety argument?
 public class TimeDelayQueue {
+    //things that need to be synchronized
+    private PriorityQueue<PubSubMessage> queue;
+    private Map<Timestamp,Integer> operationhistory;
+    //todo: time reference
 
+    private int delay;
+    Comparator byTimestamp;
     // a comparator to sort messages
     private class PubSubMessageComparator implements Comparator<PubSubMessage> {
         public int compare(PubSubMessage msg1, PubSubMessage msg2) {
@@ -23,13 +31,30 @@ public class TimeDelayQueue {
      * @param delay the delay, in milliseconds, that the queue can tolerate, >= 0
      */
     public TimeDelayQueue(int delay) {
+        byTimestamp=new PubSubMessageComparator();
+        queue=new PriorityQueue<>(byTimestamp);
+        this.delay=delay;
+        operationhistory=new TreeMap<>();
     }
 
     // add a message to the TimeDelayQueue
     // if a message with the same id exists then
     // return false
     public boolean add(PubSubMessage msg) {
-        return false;
+        synchronized (TimeDelayQueue.class) {
+            if (!contains(msg)) {
+                queue.add(msg);
+                Timestamp now=now();
+                if (operationhistory.containsKey(now)) {
+                    operationhistory.replace(now,operationhistory.get(now)+1);
+                } else {
+                    operationhistory.put(now(),1);
+                }
+
+                return true;
+            }
+            return false;
+        }
     }
 
     /**
@@ -38,13 +63,31 @@ public class TimeDelayQueue {
      * @return
      */
     public long getTotalMsgCount() {
-        return -1;
+        synchronized (TimeDelayQueue.class) {
+            try{
+                return operationhistory.values().stream().filter(x->x>0).reduce(Integer::sum).get();
+            } catch (NoSuchElementException nse) {
+                return 0;
+            }
+        }
     }
 
     // return the next message and PubSubMessage.NO_MSG
     // if there is ni suitable message
     public PubSubMessage getNext() {
-        return PubSubMessage.NO_MSG;
+        synchronized (TimeDelayQueue.class) {
+            removeExpired();
+        }
+            if (withinDelayRange(queue.peek())){
+                if (operationhistory.containsKey(now())) {
+                    operationhistory.replace(now(),operationhistory.get(now())-1);
+                } else {
+                    operationhistory.put(now(),-1);
+                }
+                return queue.poll();
+            }
+            return PubSubMessage.NO_MSG;
+
     }
 
     // return the maximum number of operations
@@ -52,7 +95,69 @@ public class TimeDelayQueue {
     // any window of length timeWindow
     // the operations of interest are add and getNext
     public int getPeakLoad(int timeWindow) {
-        return -1;
+        int maxOps=0;
+        synchronized (TimeDelayQueue.class) {
+            for (Timestamp start:operationhistory.keySet()) {
+                int currentOps=0;
+                for (Timestamp t:operationhistory.keySet()) {
+                    if (!start.equals(t) && start.getTime()-t.getTime()<=timeWindow) {
+                        currentOps+=Math.abs(operationhistory.get(t));
+                    }
+                }
+                if (currentOps>maxOps) {
+                    maxOps=currentOps;
+                }
+            }
+        }
+
+        return maxOps;
     }
 
+    //////////////
+
+    private boolean contains(PubSubMessage msg) {
+        synchronized (TimeDelayQueue.class) {
+            for (PubSubMessage ps:queue) {
+                if (ps.getId().equals(msg.getId())) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    private boolean withinDelayRange(PubSubMessage msg) {
+        return now().getTime()-msg.getTimestamp().getTime()>delay;
+        //todo find out if the units match; i think they do?
+    }
+
+    private boolean expired(PubSubMessage msg) {
+        synchronized (TimeDelayQueue.class) {
+            if (msg.isTransient() && contains(msg)) {
+                TransientPubSubMessage transientmsg=(TransientPubSubMessage) msg;
+                boolean inRange=transientmsg.getLifetime()>=
+                        now().getTime()- (transientmsg.getTimestamp().getTime());
+                if (!inRange) {
+                    queue.remove(msg);
+                }
+                return inRange;
+            } else {
+                return false;
+            }
+        }
+
+    }
+
+    private void removeExpired() {
+        synchronized (TimeDelayQueue.class) {
+            for (PubSubMessage ps:queue) {
+                expired(ps);
+            }
+        }
+
+    }
+
+    private Timestamp now() {
+        return new Timestamp(System.currentTimeMillis());
+    }
 }
